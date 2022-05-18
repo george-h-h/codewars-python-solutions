8bec5ebb2bac1aef53545640f2f24f9670d94355
George Hett <georgehett95@gmail.com>
2022-05-18 16:43:49 +0100

    fourth kata solved

---
+++ 8kyu/opposite_number/opposite_number.py
@@ -0,0 +1,4 @@
+# see https://www.codewars.com/kata/56dec885c54a926dcd001095/train/python
+
+def opposite(x):
+  return x * -1
---
+++ 8kyu/opposite_number/opposite_number_test.py
@@ -0,0 +1,10 @@
+import opposite_number as on
+
+def test1():
+  assert on.opposite(3) == -3
+
+def test2():
+  assert on.opposite(-8) == 8
+
+def test3():
+  assert on.opposite(0) == 0
